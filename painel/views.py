9f59de6e72d8d4a3f188fb8c1ed2bdedd7122691
M1guellessa <103693387+M1guellessa@users.noreply.github.com>
2026-08-19 20:55:44 -0300

    1

--- painel/views.py
+++ painel/views.py
@@ -4,18 +4,8 @@ from .models import Produto
 
 @login_required(login_url='login')
 def painel(request):
-    produtos = Produto.objects.all()
     return render(request, 'painel/painel.html')
 
 # Create your views here.
 def cadastro_produto(request):
-    if request.method == 'POST':
-        nome = request.POST.get('nome')
-        descricao = request.POST.get('descricao')
-        preco = request.POST.get('preco')
-        quantidade = request.POST.get('quantidade')
-
-        produto = Produto(nome=nome, descricao=descricao, preco=preco, quantidade=quantidade)
-        produto.save()
-
     return render(request, 'painel/cadastro_produto.html')
